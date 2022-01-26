5c0e823bebe2d4a5e403187a424ef331d58c92cd
Ap Rose <aprose5@gmail.com>
2022-01-25 20:38:34 -0500

    Create esp-line.js

---
+++ esp-line.js
@@ -0,0 +1,24 @@
+//Updated 2022-01-25
+
+const onMovementUpdateAnimation = a0_0xa13894.prototype.updateAnimation;
+const HEAD_OFFSET = 0.4;
+const RENDER_LINE_STEP = 0.001;
+const RENDER_LINE_WIDTH = 0.03;
+const COL_WHITE = new pc.Color(1, 1, 1);
+
+function renderLine(application, from, to, color) {
+  for (let i = 0; i < RENDER_LINE_WIDTH; i += RENDER_LINE_STEP) {
+    const fromOffset = new pc.Vec3(from.x + i, from.y, from.z + i);
+    const toOffset = new pc.Vec3(to.x + i, (to.y - HEAD_OFFSET), to.z + i);
+    application.renderLine(fromOffset, toOffset, color);
+  }
+}
+
+a0_0xa13894.prototype.updateAnimation = function () {
+  onMovementUpdateAnimation.apply(this, arguments);
+  const position = this.entity.getPosition().clone();
+  const enemies = this.playerManager.script.playerManager.players;
+  for (const enemy of enemies) {
+	renderLine(this.app, enemy.localPosition, position, COL_WHITE);
+  }
+}
